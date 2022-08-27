79d1b164e3ca11910f89c94f4825e69ec5b69a68
StanVard0202 <you@example.com>
2022-08-27 14:01:56 +0100

    commit

---
+++ bot.py
@@ -0,0 +1,54 @@
+import os
+
+import sys, subprocess
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-U", "discord-py-interactions"])
+
+import interactions
+from dotenv import load_dotenv 
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+PREFIXO_NORMAL = os.getenv("PREFIXO_NORMAL")
+GUILD_ID = int(os.getenv("GUILD_ID"))
+
+comprimento = len(PREFIXO_NORMAL)
+clear_prefixed_lenth = len((PREFIXO_NORMAL + "clear"))
+
+client = interactions.Client(TOKEN, intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MESSAGE_CONTENT)
+
+@client.event
+async def on_ready():
+    print(f'{client.me.name} has connected to Discord!')
+
+@client.command(
+    name="roberto",
+    description="Roberto carlos",
+    scope=GUILD_ID,
+)
+async def my_first_command(ctx: interactions.CommandContext):
+    await ctx.send("ROBERTO" , ephemeral=True)
+
+
+
+@client.event
+async def on_message_create(message):
+	if message.content[0:comprimento] == PREFIXO_NORMAL:
+		if message.content[comprimento: ] == "ping":
+			channel = await message.get_channel()
+			await message.delete()
+			await channel.send(f'O bot está com uma latência de {(round(client.latency, 3)*1000)} ms')
+		
+		if message.content[comprimento: clear_prefixed_lenth] == "clear":
+			value = int(message.content[(clear_prefixed_lenth + 1): ])
+			channel = await message.get_channel()
+			await message.channel.purge(limit=(value + 1))
+			await channel.send(f'As mensagens foram limpas do {message.channel.name}')
+
+		if message.content[comprimento: ] == "hello":
+			channel = await message.get_channel()
+			await channel.send("hey dirtbag")
+			await message.delete()
+
+
+client.start()
