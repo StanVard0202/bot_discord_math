c17f4a0d75adf7c9340e4366bc1283277d18a2f8
StanVard0202 <you@example.com>
2023-08-27 00:11:10 +0100

    Rebuid BUTAO, limpeza no codigo

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
 import os, sys, subprocess
-from energia import *
-
-state = False
 
 packages = ["python-dotenv", "discord-py-interactions", "wikipedia"]
 for i in packages:
-	if True:
+	if False:
 		subprocess.check_call([sys.executable, "-m", "pip", "install", "-U",str(i)])
 	else:
 		print("skipped")
@@ -24,7 +21,7 @@ wp.set_lang("pt")
 TOKEN = (os.getenv("TOKEN1") + os.getenv("TOKEN2"))
 PREFIXO_STAFF = ":"
 PREFIXO_RPG = "?"
-GUILD_ID = int(906962728533516319)
+GUILD_ID = int(906974139095605278)
 
 comprimento = len(PREFIXO_RPG)
 
@@ -35,61 +32,75 @@ client = interactions.Client(TOKEN, intents=interactions.Intents.DEFAULT | inter
 async def on_ready():
     print(f'{client.me.name} has connected to Discord!')
 
+global state
+#________________________________SÓ COMANDOS______________________________
 
 @client.event
-async def on_message_create(message):
+async def on_message_create(message: interactions.api.models.message.Message):
+	if message.author.id == client.me.id:  #para tratar das msg q o bot manda
+		if message.content == "Quem é o primeiro?":
+			global btn_message
+			ctx = await message.get_channel()
+			btn_message = await ctx.get_message(message.id)
+
+			
+
 	if message.content[0:len(":clear")] == ":clear":
 		amount = message.content[len(":clear"):] 
 		channel = await message.get_channel()
 		await channel.purge(int(amount)+1)
 
-	if message.content[0:len("O/a primeiro/a a clicar no butão foi o/a")] == "O/a primeiro/a a clicar no butão foi o/a":
-		global state
-		if state:
-			state = False
-			channel = await message.get_channel()
-			ctn = message.content
-			await channel.purge(2)
-			await channel.send(ctn)
-			state = False
 
 	# Comandos com o prefixo de rpg ( ? )
-	if message.content[0:comprimento] == PREFIXO_RPG:
-		if message.content[1:] == "butao":
-			await butao(message, interactions)
-			state = True
 
 		if message.content[comprimento:len(message.content)] == "list":
 			channel = await message.get_channel()
 			await channel.send("ei de fazer aqui a lista hehe")
 		
+#Anfitras
+##produçao
+@client.command(
+		name="butao",
+		description="Um botao aparece, e logo em seguida os primeiros a apertar aparecem no chat."
+)
+async def butao(ctx: interactions.CommandContext):
+    button = interactions.Button(
+        style=interactions.ButtonStyle.PRIMARY,
+        label="BUTÃO HAHAHAHA",
+        custom_id="btn",
+    )
+    await ctx.send("Quem é o primeiro?",components=button)
+    global state
+    state = True
 
 
+#Comandos gerais(slash)
+
+#@client.command(name="command-name", description="this is a command.")
+#async def command_name(ctx):
+#    print("hi")
 
 
-#Slash Commands
 @client.command(
 	name="ping",
-	description="Devolve a latência da api do bot",
-	scope=GUILD_ID)
+	description="Devolve a latência da api do bot")
 async def ping(ctx: interactions.CommandContext):
 	await ctx.send(f'O bot está com uma latência de {round(client.latency, 3)} ms')
 
-for n in ["realitas", "wikipedia"]:
-	@client.command(
-		name=n,
-		description="Pesquisa na Wikipédia sobre o assunto",
-		scope=GUILD_ID
-	)
-	@interactions.option("Assunto")
-	async def wiki(ctx: interactions.CommandContext, assunto: str):
-		await ctx.send(wp.summary(str(assunto), sentences=1))
+
+
+@client.command(
+	name="realitas",
+	description="Pesquisa na Wikipédia sobre o assunto")
+@interactions.option("Assunto")
+async def wiki(ctx: interactions.CommandContext, assunto: str):
+	await ctx.send(wp.summary(str(assunto), sentences=1))
+
+
 
 @client.command(
 		name="veritatis",
-		description="Pesquisa na Wiki do Ordem sobre o assunto",
-		scope=GUILD_ID
-	)
+		description="Pesquisa na Wiki do Ordem sobre o assunto")
 @interactions.option("Assunto")
 async def ordem(ctx: interactions.CommandContext, assunto: str):
 	await ctx.send("https://ordemparanormal.fandom.com/wiki/"+ str(assunto))
@@ -98,9 +109,7 @@ async def ordem(ctx: interactions.CommandContext, assunto: str):
 
 @client.command(
 		name="energia",
-		description="Escolhe uma opção aleatória de todas as 5 enviadas ()",
-		scope=GUILD_ID
-	)
+		description="Escolhe uma opção aleatória de todas as 5 enviadas ()")
 @interactions.option("Uma das opções")
 @interactions.option("Uma das opções")
 @interactions.option("Uma das opções")
@@ -114,22 +123,17 @@ async def ordem(ctx: interactions.CommandContext, opcao1:str, opcao2:str, opcao3
 
 
 
-	
-
+#components
 @client.component("btn")
 async def butao_energia(ctx: interactions.ComponentContext):
+    await btn_message.delete()
     await ctx.send("O/a primeiro/a a clicar no butão foi o/a"+ ctx.user.mention)
+    
 
 
 
-
-
-
-
-
-
+ 
 client.start()
-
 
 
 
